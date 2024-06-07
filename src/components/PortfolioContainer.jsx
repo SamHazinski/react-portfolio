1fa74b8d33010700d29366ff5a9e459d84164a13
Sam Hazinski <shazinski@gmail.com>
2024-06-07 16:35:34 -0400

    Added inital contact page

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
+import Home from './pages/Resume';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
+    if (currentPage === 'Resume') {
       return <Home />;
     }
     if (currentPage === 'About') {
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,20 +1,43 @@
+import {useState} from "react";
+const ContactForm = () => {
+  const [email, setEmail] = useState("");
+  const [content, setContent] = useState("");
+  
+
+  const send = (e) =>{
+      e.preventDefault();
+      alert(`Email sent from ${email}, with the message of ${content}`)
+  } 
+
+}
 export default function Contact() {
-    return (
+  const send = (e) =>{
+    e.preventDefault();
+    
+    console.log()
+} 
+  
+  return (
       <div>
         <h1>Contact Page</h1>
         <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+         If you would like to contact me, please reach out via the contact info at the bottom of the page or put a message here.
         </p>
+        <form onSubmit={send}>
+          <div>
+            <label>Email:</label>
+            <input></input>
+            
+          </div>
+          <div>
+            <p>Brevity is the soul of wit.</p>
+          </div>
+          <div>
+          <label>Message</label>
+          <input></input>
+          </div>
+          <button className="button" type="submit" >Submit </button>
+        </form>  
       </div>
     );
   }
